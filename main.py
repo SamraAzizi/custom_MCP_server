f9185e0ae953f0018f0e5811d5e1a1cefc932c93
SamraAzizi <samarraazizi@gmail.com>
2025-04-16 09:53:27 -1000

    commit

--- main.py
+++ main.py
@@ -1,20 +1,3 @@
 from mcp.server.fastmcp import FastMCP
 
 
-mcp = FastMCP("AI Sticky Notes")
-
-NOTES_FILE = "notes.txt"
-
-def ensure_file():
-    if not os.path.exists(NOTES_FILE):
-        with open(NOTES_FILE, "w") as f:
-            f.write("")
-
-@mcp.tool()
-
-
-def add_note(message: str) -> str:
-    ensure_file()
-    with open(NOTES_FILE, "a") as f:
-        f.write(message + "\n")
-        return "Note Saved!"
